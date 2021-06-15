b9e6f9c6ddcf7471f815f9d4734c063b51662413
enrico <enrico.tabanelli3@unibo.it>
2021-06-15 11:24:21 +0200

    Initial Commit

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 repository="https://github.com/pulp-platform/pulp-sdk.git"
 git clone $repository
 gcc main.c -o main
-./main
+#./main
 #export PULP_RISCV_GCC_TOOCHAIN=/home/etabanelli/
 #cd pulp-sdk
 #source configs/pulp-open.sh
