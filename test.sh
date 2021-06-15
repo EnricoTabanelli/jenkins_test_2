a608527d90629a4c53bcf06ea7e1b082b1a741a0
enrico <enrico.tabanelli3@unibo.it>
2021-06-15 11:20:27 +0200

    Initial Commit

--- test.sh
+++ test.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-pwd
+
 repository="https://github.com/pulp-platform/pulp-sdk.git"
 git clone $repository
-mv pulp-sdk /home/etabanelli/Desktop
-RESULT=1
-echo "$RESULT"
-
-
-
-if [ "$RESULT" = 1 ]; then
-	echo "OK"
-	exit 0
-else
-	echo "NIENTE"
-	exit 1
-fi
-
+fort
+#export PULP_RISCV_GCC_TOOCHAIN=/home/etabanelli/
+#cd pulp-sdk
+#source configs/pulp-open.sh
+#make build
 
